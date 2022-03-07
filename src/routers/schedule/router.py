9aa6396aa2de140a0c535c60a30a2662aa8c2ecb
vini <vro@lionx.com.br>
2022-03-06 23:02:38 -0300

    created post route

--- src/routers/schedule/router.py
+++ src/routers/schedule/router.py
@@ -1,18 +1,22 @@
 from src.repositories.mongo import repository
 from src.services.schedule.contacts import ContactsService
+from src.domain.validators.contacts import validator
 
-from flask import request
+from flask import request, Response
 from flask_restful import Resource
+from json import dumps
 
 
 class HelloWord(Resource):
     """ESSA É A ROTA DE BOAS-VINDAS"""
+
     def get(self):
         return "OLA MUNDOOO"
 
 
 class AgendaListarTodos(Resource):
     """EXIBIR TODOS OS CONTATOS"""
+
     # @spec.validate(resp=Response(HTTP_200=Contato))
     def get(self):
         return ContactsService.get_all()
@@ -20,25 +24,44 @@ class AgendaListarTodos(Resource):
 
 class AgendaListarUmContato(Resource):
     """EXIBIR UM CONTATO POR ID - (ID É UMA STRING)"""
+
     def get(self, id):
         return ContactsService.get_by_id(id)
 
 
 class AgendaListarPorLetra(Resource):
     """BUSCAR CONTATO PELA PRIMEIRA LETRA DO NOME"""
+
     def get(self, letters):
         return ContactsService.get_by_letters(letters)
 
 
 class AgendaCadastrarContato(Resource):
     """CADASTRAR CONTATO"""
+
     def post(self):
-        new_contact = request.get_json()
-        return ContactsService.register(new_contact)
+        try:
+            new_contact = request.get_json()
+            response = ContactsService().register(new_contact)
+            return Response(
+                dumps(response),
+                mimetype="application/json",
+                status=201
+            )
+        except Exception as error:
+            response = {
+                "message": str(error),
+            }
+            return Response(
+                dumps(response),
+                mimetype="application/json",
+                status=403
+                )
 
 
 class AgendaEditarContato(Resource):
     """EDITAR CONTATO POR ID - (ID É UMA STRING)"""
+
     def put(self, id):
         contato_editar = request.get_json()
         contato_editado = repository.update_contact(contato_editar, id)
@@ -50,12 +73,13 @@ class AgendaEditarContato(Resource):
 
 class AgendaExcluirContato(Resource):
     """EXCLUIR CONTATO POR ID - (ID É UMA STRING)"""
+
     def delete(self, id):
         try:
             repository.soft_delete_contact(id)
             return "Removido com sucesso", 200
         except Exception:
-            return 'Não é possível excluir, contato não foi encontrado', 404
+            return "Não é possível excluir, contato não foi encontrado", 404
 
 
 class AgendaCountPhonesByType(Resource):
